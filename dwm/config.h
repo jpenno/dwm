8ace796bf617781f745d4718977af353a90cc1fe
jp <jpenno123@hotmail.com>
2021-10-28 02:17:36 +1100

    add show jp or eng to bar and remove sys tray

--- dwm/config.h
+++ dwm/config.h
@@ -7,12 +7,12 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "fira code:size=16" };
-static const char dmenufont[]       = "fira code:size=16";
+static const char *fonts[]          = { "fira code:size=18" };
+static const char dmenufont[]       = "fira code:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
